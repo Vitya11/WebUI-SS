2edef02057d9bd3e2b5c2cc871772c703fe31951
vitya11 <Ofir.cv@gmail.com>
2015-09-16 10:36:48 +0300

    start

---
+++ Task1/js/main.js
@@ -0,0 +1,12 @@
+﻿var Person = Backbone.Model.extend ({
+	defaults : {
+		name :"viktor",
+		age : 26,
+		job : "developer"
+	},
+	
+	walk : function(){
+		return this.get("name") + " is walkimg";
+	}
+ });
+ 
